ebf879dc205a486be2123e14014659c51a432d83
Arsenic-gg <arsenicbests@gmail.com>
2025-05-27 19:58:54 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5150,7 +5150,7 @@ function Library:CreateWindow(WindowInfo)
                 ImageRectOffset = IconData.ImageRectOffset,
                 ImageRectSize = IconData.ImageRectSize,
                 Size = UDim2.fromOffset(16, 16),
-                Position = UDim2.fromOffset(0, 6),
+                Position = UDim2.fromOffset(0, 12),
                 Parent = GroupboxLabel,
             })
             GroupboxLabel.Text = "  " .. Info.Name
